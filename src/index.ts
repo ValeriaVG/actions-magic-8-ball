f0bdc59c6f75f41f390fb0cbdfcae87e4cb3c577
Valeria <valeria.viana.gusmao@gmail.com>
2023-04-27 19:34:09 +0200

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { context, getOctokit } from "@actions/github";
+import { getInput } from "@actions/core";
+import generateEightBallResponse from "./generateResponse";
+
+const run = async () => {
+  const prefix: string = getInput("prefix");
+  const github_token: string = getInput("GITHUB_TOKEN");
+  if (
+    !context.payload.comment ||
+    !context.payload.comment["body"]?.startsWith(prefix)
+  ) {
+    return;
+  }
+  const issue_number =
+    context.payload.pull_request?.number || context.payload.issue?.number;
+  if (!issue_number) {
+    return;
+  }
+
+  const octokit = getOctokit(github_token);
+  const body =
+    '> ' + context.payload.comment["body"] + '\n\r' +
+    "@" +
+    context.payload.comment["user"].login +
+    " " +
+    generateEightBallResponse();
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number,
+    body,
+  });
+};
+
+run();
